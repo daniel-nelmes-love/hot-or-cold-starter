c245963482b5e3bdc8d6a3eeb6c773a950759eed
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2014-12-27 23:52:40 +1300

    added random number generator; set userGuess variable; created if function to give feedback if guess was empty or append guess to guess

--- js/app.js
+++ js/app.js
@@ -12,6 +12,28 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
+  	var randNum = Math.floor((Math.random() * 100) + 1);
+  	console.log(randNum);
+
+  	var userGuess = document.getElementById('userGuess');
+
+	$("#guessButton").on("click", function(){
+		if (userGuess.value.trim()==0) {
+			$("#feedback").text("You did not enter a guess");
+		} else {
+		  	$("#guessList").append("<li>" + userGuess.value + "</li>");
+	  		console.log(userGuess.value);
+	  		$('#userGuess').val(null);
+	  	};
+	});
+
+
+
+
+
+
 });
 
 
