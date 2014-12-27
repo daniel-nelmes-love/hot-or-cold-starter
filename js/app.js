cf73db743da322857e3c59d19345c0b4374cf7cb
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2014-12-28 00:20:48 +1300

    changed feedback for empty to include number outside of range; added 'Ice cold' feedback for guesses 50 higher or lower than randNum

--- js/app.js
+++ js/app.js
@@ -12,19 +12,23 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-
-
+  	// Global variables //
   	var randNum = Math.floor((Math.random() * 100) + 1);
   	console.log(randNum);
-
   	var userGuess = document.getElementById('userGuess');
+  	var userCount = 0;
 
 	$("#guessButton").on("click", function(){
-		if (userGuess.value.trim()==0) {
-			$("#feedback").text("You did not enter a guess");
+		if (userGuess.value < 1 || userGuess.value > 100 || userGuess.value === "0") {
+			$("#feedback").text("Make a guess between 1 and 100");
+			$('#userGuess').val(null);
 		} else {
 		  	$("#guessList").append("<li>" + userGuess.value + "</li>");
 	  		console.log(userGuess.value);
+	  		$('#count').text(userCount+=1);
+	  		if((randNum - userGuess.value) > 50 || (randNum - userGuess.value) < -50) {
+	  			$("#feedback").text("Ice cold!");
+	  		}
 	  		$('#userGuess').val(null);
 	  	};
 	});
